410dbda266889948298e86a71880bff3368cbd7b
Richard Albornoz <richard_albor4@hotmail.com>
2022-01-14 10:58:17 -0300

    Proyecto Js

---
+++ js/datos.js
@@ -0,0 +1,65 @@
+var productos= [
+  {
+    "id": 1,
+     "nombre": "ABSOLUT Raspberri 750 cc",
+     "precio": 650,
+     "destacado": true,
+     "imagen": "./img/VodkaR.jfif",
+  },
+  {
+    "id": 2,
+     "nombre": "ABSOLUT India 750cc",
+     "precio": 850,
+     "destacado": true,
+     "imagen": "./img/VodkaIndia.jfif"
+  },
+  {
+    "id": 3,
+     "nombre": "ABSOLUT Mexico 750 cc",
+     "precio": 900,
+     "destacado": true,
+     "imagen": "./img/VodkaMexico.jfif"
+  },
+  {
+    "id": 4,
+     "nombre": "ABSOLUT Elyx 1000cc",
+     "precio": 1550,
+     "destacado": true,
+     "imagen": "./img/VodkaElyx.jfif"
+  },
+  {
+    "id": 5,
+     "nombre": "ABSOLUT Rainbow 750cc",
+     "precio": 900,
+     "destacado": true,
+     "imagen": "./img/VodkaRainbow.jfif"
+  },
+  {
+    "id": 6,
+     "nombre": "ABSOLUT Oak 750cc",
+     "precio": 2500,
+     "destacado": true,
+     "imagen": "./img/VodkaOak.jfif"
+  },
+  {
+    "id": 7,
+     "nombre": "Vodka Smirnof 700 cc",
+     "precio": 500,
+     "destacado": false,
+     "imagen": "./img/voka.pgn"
+  },
+  {
+    "id": 8,
+     "nombre": "ABSOLUT Pack",
+     "precio": 4500,
+     "destacado": true,
+     "imagen": "./img/VodkaPack.jfif"
+  },
+  {
+    "id": 9,
+     "nombre": "ABSOLUT Combo",
+     "precio": 3500,
+     "destacado": true,
+     "imagen": "./img/VodkaCombo.jfif"
+  },
+]
